67ab048b853c49d4233d14b082b56632878525b7
Ravindra Kumawat <ravindra.kumawat7676@gmail.com>
2026-01-20 17:01:42 +0000

    Problem: minDepth using dfs and bfs

--- leetcode/minDepth.js
+++ leetcode/minDepth.js
@@ -59,3 +59,35 @@ var minDepth = function(root) {
 
 // BFS TC: O(N)
 // BFS SC: O(W) W is the maximum width of the tree
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+  if (root == null) return 0;
+  const queue =[root];
+  let height = 1;
+  while (queue.length > 0){
+    const size = queue.length;
+    for (let i =0; i < size ; i++){
+      const node = queue.shift();
+          
+      if (node.left == null && node.right == null) return height;
+
+      if (node.left !== null) queue.push(node.left);
+      if (node.right !== null) queue.push(node.right);  
+    }
+    height++;
+  }
+  return height;
+};
