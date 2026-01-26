e0b1a0e3dce10d9ea1c889bb1cbc523bd7447d66
Ravindra Kumawat <ravindra.kumawat7676@gmail.com>
2026-01-26 16:11:04 +0000

    Problem: Pathsum

---
+++ leetcode/pathSum.js
@@ -0,0 +1,29 @@
+/**
+ * 112. Path Sum
+ * https://leetcode.com/problems/path-sum/description/?source=submission-ac
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPaths = function (root, sum, tar) {
+    if(root == null) {
+        return false
+    } 
+    sum = sum + root.val
+    if(root.left == null && root.right == null) {
+        return sum === tar
+    }
+    return hasPaths(root.left, sum, tar) || hasPaths(root.right, sum, tar)
+}
+
+var hasPathSum = function(root, targetSum) {
+    return hasPaths(root, 0, targetSum)
+};
