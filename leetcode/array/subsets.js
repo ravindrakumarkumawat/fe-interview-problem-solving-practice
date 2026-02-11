324eda4a076bd012300c911451b7260634500abf
Ravindra Kumawat <ravindra.kumawat7676@gmail.com>
2026-02-11 15:49:54 +0000

    Problem: subsets

---
+++ leetcode/array/subsets.js
@@ -0,0 +1,18 @@
+/**
+ * 78. Subsets
+ * https://leetcode.com/problems/subsets/description/
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    let outer = [[]];
+    for (let num of nums) {
+        let n = outer.length;
+        for (let i = 0; i < n; i++) {
+            let internal = outer[i].slice(); // Copy existing subset
+            internal.push(num);
+            outer.push(internal);
+        }
+    }
+    return outer;
+};
