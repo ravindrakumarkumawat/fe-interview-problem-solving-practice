e67a2e43d3e5cd1e3b8df0239b8371e2325ac4a1
Ravindra Kumawat <ravindra.kumawat7676@gmail.com>
2026-02-26 16:49:50 +0000

    problem: lengthOfLongestSubstring

---
+++ leetcode/array/lengthOfLongestSubstring.js
@@ -0,0 +1,23 @@
+/**
+ * leetcode 3. Longest Substring Without Repeating Characters
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let left = 0;
+    let maxLength = 0;
+    let charSet = new Set();
+
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++;
+        }
+
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;    
+}
